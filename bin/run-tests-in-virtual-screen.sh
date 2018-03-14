e60fca1d73c201113ec62b0742f30a97d551f45d
Christian Kleinhuis <ck@digitalgott.de>
2018-03-14 23:14:30 +0100

    print date before and after

--- bin/run-tests-in-virtual-screen.sh
+++ bin/run-tests-in-virtual-screen.sh
@@ -14,11 +14,15 @@ echo "[INFO] Excludes Tags [${ROBOT_EXCLUDE_TAG}]  "
 echo "[INFO] Log level [${ROBOT_LOGLEVEL}]  "
 echo "[INFO] Other options [${ROBOT_OPTIONS}]  "
 # xvfb-run --server-args="-screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_COLOUR_DEPTH} -ac"
+
+echo "[INFO] Start ${date}"
+
 robot  -L ${ROBOT_LOGLEVEL} -i "${ROBOT_INCLUDE_TAG}"  -e "${ROBOT_EXCLUDE_TAG}"  -c "${ROBOT_CRITICAL_TAG}" -n "${ROBOT_NONCRITICAL_TAG}" --xunit xunit.xml \
 	--outputDir ${REPORTDIR}  ${ROBOT_OPTIONS} /opt/robotframework/tests
 # copy chromedriver logs to output
 
 
+echo "[INFO] End ${date}"
 
 
 if [ "${LOG_LEVEL}" = "vv" ]
